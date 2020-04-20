82e45f0a0137f54ae5611ff93fb09b35b8162cfc
Daniel <danielcu.sanchez@gmail.com>
2020-04-20 16:53:00 -0500

    Ciclos

--- index.js
+++ index.js
@@ -79,9 +79,9 @@ function suma(numero1, numero2) {
     return resultado
 }
 
-saludar()
+//saludar()
 
-let calculadora = {
+let arary = {
     suma(n1, n2) {
         return n1 + n2
     },
@@ -99,4 +99,37 @@ let calculadora = {
 // console.log( calculadora.suma(1,2) )
 
 
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 100]
 
+// contador se inicializa
+// condicion
+// incremento
+
+
+
+// for (let contador = 0; contador < array.length; contador = contador + 1) {
+//     //Ejecutas esto
+//     console.log(array[contador])
+// }
+
+
+function saludar(nombre, i) {
+    console.log(`Hola ${nombre} estoy en la posicion ${i}`)
+}
+
+const listaNombres = ["Juan", "Ramon", "Andrea", "Samantha", "Pedro", "Julio"]
+
+for (let i = 0; i < listaNombres.length; i++) {
+    saludar(listaNombres[i], i)
+}
+
+//Ciclo que imprima los numeros del 1 -100 
+
+
+//ciclo que imprima los nombres del una lista de 10 elementos
+
+
+
+//Ciclo que imprima los numeros pares de entre 1-100
+
+// que es el modulo y residuo y el if
